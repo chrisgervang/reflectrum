bfcccf1c483f8e80ba5e32e36a1ae01c475bd3b9
Michelle Chan <your.michellechan.me@gmail.com>
2016-01-01 16:56:34 -0800

    GM Opening (boom done)

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ app.on('ready', function() {
     mainWindow = new BrowserWindow(browserWindowOptions);
 
     // and load the index.html of the app.
-    mainWindow.loadURL('http://127.0.0.1:5000/quotes');
+    mainWindow.loadURL('http://127.0.0.1:5000/gmopening');
 
     // Open the DevTools.
     mainWindow.webContents.openDevTools();

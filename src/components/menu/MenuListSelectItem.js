b0100e058a23f59b1a88ea79ff933eea670477c3
Chris Gervang <chrisg@tachyus.com>
2016-02-07 14:34:12 -0800

    remove hack.

--- src/components/menu/MenuListSelectItem.js
+++ src/components/menu/MenuListSelectItem.js
@@ -5,36 +5,36 @@ class MenuListSelectItem extends Component {
   constructor(props) {
     super(props);
     console.log(this.props);
-    this.state = {
-      listLocation: 0
-    }
+    // this.state = {
+    //   listLocation: 0
+    // }
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.event !== this.props.event;
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   return nextProps.event !== this.props.event;
+  // }
 
   render() {
 
-    if(this.props.event != NO_EVENT) {
-      console.log(this.props.event);
-    }
-
-    if(this.props.event === UP_CLICK) {
-      if (this.state.listLocation !== 0) {
-        this.setState({listLocation: this.state.listLocation - 1});
-      }
-    }
-
-    if(this.props.event === DOWN_CLICK) {
-      if (this.state.listLocation !== this.props.items.length - 1) {
-        this.setState({listLocation: this.state.listLocation + 1});
-      }
-    }
+    // if(this.props.event != NO_EVENT) {
+    //   console.log(this.props.event);
+    // }
+    //
+    // if(this.props.event === UP_CLICK) {
+    //   if (this.props.listLocation !== 0) {
+    //     this.setState({listLocation: this.props.listLocation - 1});
+    //   }
+    // }
+    //
+    // if(this.props.event === DOWN_CLICK) {
+    //   if (this.props.listLocation !== this.props.items.length - 1) {
+    //     this.setState({listLocation: this.props.listLocation + 1});
+    //   }
+    // }
 
     var styles = {
       selectorLocation: {
-        top: (this.state.listLocation * 200) + 280 + 'px'
+        top: (this.props.listLocation * 200) + 280 + 'px'
       }
     }
 

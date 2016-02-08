ba0ebfd09a284b956f1f4434da09a93d2b21f1c9
Chris Gervang <chrisg@tachyus.com>
2016-02-08 00:00:35 -0800

    redux and keyboard events are now working!

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,36 +1,9 @@
 import React, { Component } from 'react';
 import Menu from './menu/Menu';
-
-var data = {
-  message: "Goodmorning, Michelle",
-  items: [
-      {
-        name: "screensavers",
-        color: "#00F5EA"
-      },
-      {
-        name: "weather",
-        color: "#F2F4FF"
-      },
-      {
-        name: "clock",
-        color: "#FF9500"
-      },
-      {
-        name: "calendar",
-        color: "#FF3B30"
-      },
-      {
-        name: "quotes",
-        color: "#787AFF"
-      }
-  ],
-  listLocation: 0
-}
-
+import { store } from '../main'
 class MainMenu extends Component {
   render() {
-    return <Menu {...this.props} {...data}/>
+    return <Menu {...store.getState().mainMenu}/>
   }
 }
 

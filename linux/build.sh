537f7e6dd04f8e29ae5b18c4fdaf78c2357551ed
Chris Gervang <chrisg@tachyus.com>
2016-03-26 18:28:03 -0700

    startup

--- linux/build.sh
+++ linux/build.sh
@@ -2,6 +2,5 @@
 PROJECT_PATH=/home/pi/code/Reflectrum
 jspm bundle ${PROJECT_PATH}/src/main --inject
 
-#npm release /home/pi/code/Reflectrum
 #jspm install npm:clean-css --dev
 #jspm bundle-sfx src/main.js dist/app.js

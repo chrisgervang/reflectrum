242fae410ad8f26cb2a61efb227c0022e94c3b4d
Chris Gervang <chrisg@tachyus.com>
2016-03-28 02:07:38 -0700

    90 degree rotate

--- linux/vert_screen.sh
+++ linux/vert_screen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#insert display configuration. You may want display_rotate=1 depending on how you built the mirror.
+#insert display configuration. You may want different rotation depending on how you built the mirror.
 echo "" >> /boot/config.txt
-echo "# rotate display 270 degrees" >> /boot/config.txt
-echo "display_rotate=3" >> /boot/config.txt
+echo "# rotate display 90 degrees" >> /boot/config.txt
+echo "display_rotate=1" >> /boot/config.txt
 
 #sed '1i # rotate display 90 degrees\ndisplay_rotate=1' /boot/config.txt > /boot/config.txt

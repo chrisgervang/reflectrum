0663e5ccfd31ddbf00b8b88edcd34a109c27c0a5
Chris Gervang <chrisgervang@users.noreply.github.com>
2016-04-16 20:03:29 -0700

    Revert "Refactor"

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
 
-//http server
-var http = require('http');
-var path = require('path');
-var fs = require('fs');
-
 // Report crashes to our server.
 require('crash-reporter').start();
 
@@ -30,18 +25,11 @@ app.on('window-all-closed', function() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
-
-  function requestHandler(req, res) {
-    var
-    file    = req.url == '/' ? '/app.html' : req.url,
-    root    = __dirname + '/',
-    page404 = root + '/404.html';
-    console.log((root + file))
-    getFile((root + file), res, page404);
-  };
-
-  // start a simple server that can serve an html page and resolve local file loading.
-  var server = http.createServer(requestHandler).listen(3000);
+  // call python?
+  console.log("Yo!");
+  var subpy = require('child_process').spawn('python', ['./server.py']);
+  var rq = require('request-promise');
+  var mainAddr = 'http://127.0.0.1:5000/menu';
 
   // Put the app on a secondary display if availalbe
   var atomScreen = require('screen');
@@ -62,7 +50,6 @@ app.on('ready', function() {
     darkTheme:true,
     "node-integration": false
   };
-
   if (!!externalDisplay) {
     browserWindowOptions.x = externalDisplay.bounds.x + 50;
     browserWindowOptions.y = externalDisplay.bounds.y + 50
@@ -73,11 +60,10 @@ app.on('ready', function() {
     mainWindow = new BrowserWindow(browserWindowOptions);
 
     // and load the index.html of the app.
-    mainWindow.loadURL("http://localhost:3000/app.html");
+    mainWindow.loadURL('http://127.0.0.1:5000/menu');
 
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
-    // BrowserWindow.addDevToolsExtension('~/code/react-devtools/shells/plain');
 
 
     // Emitted when the window is closed.
@@ -89,29 +75,18 @@ app.on('ready', function() {
     });
   };
 
-  function getFile(filePath, res, page404) {
-
-    fs.exists(filePath, function(exists) {
-      if(exists) {
-        fs.readFile(filePath, function(err, contents) {
-          if(!err) {
-            res.end(contents);
-          } else {
-            console.dir(err);
-          }
-        });
-      } else {
-        fs.readFile(page404, function(err, contents) {
-          if(!err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            res.end(contents);
-          } else {
-            console.dir(err);
-          }
-        });
-      }
-    });
+  var startUp = function(){
+    rq(mainAddr)
+      .then(function(htmlString){
+        console.log('server started!');
+        openWindow();
+      })
+      .catch(function(err){
+        //console.log('waiting for the server start...');
+        startUp();
+      });
   };
 
-  openWindow();
+  // fire!
+  startUp();
 });
